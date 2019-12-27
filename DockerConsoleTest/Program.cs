a09f1fef4090e1ab9f9578b6c8101ece619ef511
Jins Peter <poulosekutty0@gmail.com>
2019-12-28 03:04:35 +0530

    Add project files.

---
+++ DockerConsoleTest/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace DockerConsoleTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int counter = 0;
+            int max = args.Length != 0 ? Convert.ToInt32(args[0]) : -1;
+            while (max == -1 || counter < max)
+            {
+                counter++;
+                Console.WriteLine($"Counter: {counter}");
+                System.Threading.Tasks.Task.Delay(1000).Wait();
+            }
+        }
+    }
+}
